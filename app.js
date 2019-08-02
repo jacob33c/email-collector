9192143aaac57bcc6a9a1b06930882276ce7973d
Jacob Fraizer <jacob33c@gmail.com>
2019-08-01 22:17:56 -0700

    index had two dots

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ const request = require("request");
 const app = express();
 
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function(){
   console.log("server is running on server 3000");
 });
 
+app.post("/", function(req,res){
+  var firstName = req.body.firstName;
+  var lastName =  req.body.lastName;
+  console.log(firstName);
+});
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/index.html");
