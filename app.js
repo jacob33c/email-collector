be58e9e94ff552b53d42300d8220e66c55caa9ae
Jacob Fraizer <jacob33c@gmail.com>
2019-08-06 22:16:11 -0700

    add success and failure pages

--- app.js
+++ app.js
@@ -18,9 +18,62 @@ app.listen(3000, function(){
 app.post("/", function(req,res){
   var firstName = req.body.firstName;
   var lastName =  req.body.lastName;
-  console.log(firstName);
+  var email = req.body.email;
+
+  var data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  app.post("/failure" , function(req,res){
+    res.redirect("/");
+  });
+
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us15.api.mailchimp.com/3.0/lists/e6bffd1a68",
+    method: "post",
+    headers: {
+        "Authorization" : "jacob1 eb9c14066cc5955578d72c08859e161c-us15"
+    },
+    body: jsonData
+  };
+
+
+  request(options, function(error, response, body){
+    if (error){
+      res.sendFile(__dirname + "/failure.html");
+      console.log(error);
+    }
+    else {
+      if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+        console.log(response.statusCode);
+      }
+      else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+
 });
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
+
+
+
+// eb9c14066cc5955578d72c08859e161c-us15
+
+
+// fb1353f1ea
